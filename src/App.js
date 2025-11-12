ec4da0168870791941719865e607b4ece4d01c46
SantinoCuello <cuellosantino11@gmail.com>
2025-11-12 16:39:18 -0300

    feat: Game function as a father of Board

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { use, useState } from 'react';
 
 function Square({value, onSquareClick}){
   return(
@@ -8,13 +8,31 @@ function Square({value, onSquareClick}){
     );
 }
 
-export default function Board() {
-
+export default function Game(){
   const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1 ];
+
+  function handlePlay(nextSquares){
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />  
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
+
+function Board(xIsNext, squares, onPlay) {
 
   function handleClick(i){
-    
+
     if(squares[i] || calculateWinner(squares)){
       return;
     }
@@ -23,8 +41,7 @@ export default function Board() {
 
     xIsNext ? nextSquares[i] = 'X' : nextSquares[i] = 'O'
 
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
